e33d4cb5f4b09f54ce6cec2a197a40e106948e60
Suyash Joshi <suyash.joshi179@gmail.com>
2026-03-30 20:47:29 -0400

    Asset updates from professor (#4)

--- tools/sourceme.sh
+++ tools/sourceme.sh
@@ -6,7 +6,6 @@ export PATH="/opt/shared/riscv-toolchain/bin:/opt/shared/qemu/bin:$PATH"
 
 # Rust toolchain
 export RUSTUP_HOME="/opt/shared/rust"
-export CARGO_HOME="/opt/shared/cargo"
 export PATH="/opt/shared/cargo/bin:$PATH"
 
 # Zig compiler
