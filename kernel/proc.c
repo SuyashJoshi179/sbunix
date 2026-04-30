46102d430069bf700688b1a0aac44c21980c414d
Suyash Joshi <suyash.joshi179@gmail.com>
2026-04-30 16:01:39 +0000

    proc: add generation counter + proc_find_by_pid

--- kernel/proc.c
+++ kernel/proc.c
@@ -37,6 +37,13 @@ static struct context sched_context;
 struct pcb *current_proc(void)   { return current; }
 struct pcb *proc_list_head(void) { return procs;   }
 
+struct pcb *proc_find_by_pid(int pid) {
+    for (struct pcb *p = proc_list_head(); p; p = p->next) {
+        if (p->pid == pid && p->state != PROC_UNUSED) return p;
+    }
+    return 0;
+}
+
 static void proc_unlink(struct pcb *victim) {
     struct pcb *prev = 0;
     int found = 0;
@@ -112,6 +119,8 @@ struct pcb *alloc_proc(void) {
     }
 
     p->pid        = next_pid++;
+    static uint64_t generation_seq = 0;
+    p->generation = ++generation_seq;
     p->parent_pid = 0;
     p->exit_status= 0;
     p->state      = PROC_UNUSED;
