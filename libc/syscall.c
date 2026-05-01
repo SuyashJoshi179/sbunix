06f507d1ac1813bb3bf1db086044814e42e3281a
Jiayi Xiong <xiongjy02930@gmail.com>
2026-05-01 17:51:14 +0000

    feat(fs): add atomic rename() syscall

--- libc/syscall.c
+++ libc/syscall.c
@@ -155,6 +155,10 @@ int link(const char *oldpath, const char *newpath) {
     return (int)ecall2(25, (long)oldpath, (long)newpath);
 }
 
+int rename(const char *oldpath, const char *newpath) {
+    return (int)ecall2(26, (long)oldpath, (long)newpath);
+}
+
 int pipe(int fds[2]) {
     return (int)syscall_ret(ecall1(23, (long)fds));
 }
