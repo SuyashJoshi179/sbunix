ab9fdb80aa9db4b7269f3281264b1194cae353f7
Suyash Joshi <suyash.joshi179@gmail.com>
2026-05-05 05:49:00 +0000

    Restore develop baseline files

--- tools/sourceme.sh
+++ tools/sourceme.sh
@@ -6,6 +6,7 @@ export PATH="/opt/shared/riscv-toolchain/bin:/opt/shared/qemu/bin:$PATH"
 
 # Rust toolchain
 export RUSTUP_HOME="/opt/shared/rust"
+export CARGO_HOME="/opt/shared/cargo"
 export PATH="/opt/shared/cargo/bin:$PATH"
 
 # Zig compiler
