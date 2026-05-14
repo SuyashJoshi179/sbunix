f871cbd1223b90dc6c71a51b35a054ab41006e5b
Suyash Joshi <suyash.joshi179@gmail.com>
2026-05-14 17:44:58 +0000

    test(posix): conformance pins for <sys/utsname.h> and <signal.h>
    signal.h divergence: pid_t not exposed (header missing sys/types.h
    include). kill/killpg pins commented pending Phase 2 fix.

--- tests/posix/sys_utsname.c
+++ tests/posix/sys_utsname.c
@@ -17,7 +17,8 @@ PIN int (*_pin_uname)(struct utsname *) = uname;
 
 __attribute__((unused))
 static void _struct_fields(void) {
-    struct utsname u = {{0}};
+    struct utsname u;
+    __builtin_memset(&u, 0, sizeof u);
     (void)u.sysname[0]; (void)u.nodename[0]; (void)u.release[0];
     (void)u.version[0]; (void)u.machine[0];
 }
